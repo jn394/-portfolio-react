65a7d294427ca5ce1276f010dcae91406f9fb6fb
unknown <jayng168@gmail.com>
2019-02-12 17:26:40 -0500

    Fixes

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -4,7 +4,7 @@ const Jumbotron = props => {
     return (
         <div className="jumbotron jumbotron-fluid shadow-lg">
             <div className="container jumboText">
-                <h1 className="display-3">Google Search</h1>
+                <h1 className="display-3">SOMETHING</h1>
                 <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
             </div>
         </div>
